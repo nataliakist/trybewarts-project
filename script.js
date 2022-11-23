0e0827fa918340eed16e5faa3a09d24cc244f2d5
Natália Lauschner Kist <natalialkist@hotmail.com>
2022-11-22 21:20:52 -0300

    Requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const button = document.querySelector('#access-button');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const agreement = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
 
 button.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+agreement.addEventListener('click', () => {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
   }
 });
