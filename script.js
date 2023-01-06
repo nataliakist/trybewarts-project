802ddc69e6b6c2a18c04862ba3c6f431f6304d33
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-01-05 23:05:13 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const agreement = document.querySelector('#agreement');
 const submitButton = document.querySelector('#submit-btn');
+const textArea = document.querySelector('#textarea');
+const characCounter = document.querySelector('#counter');
+characCounter.innerHTML = 500;
 
 button.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,6 +14,17 @@ button.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+const charactersCounter = (e) => {
+  const maxCharac = 500;
+  const inputLength = textArea.value.length;
+  characCounter.innerHTML = maxCharac - inputLength;
+  if (inputLength >= maxCharac) {
+    e.preventDefault();
+  }
+};
+
+textArea.addEventListener('input', charactersCounter);
 
 agreement.addEventListener('click', () => {
   if (agreement.checked) {
